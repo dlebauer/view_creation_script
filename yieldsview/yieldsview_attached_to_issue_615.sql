74dc1d3ef46d87204c4842f860ad7a7fea6020f1
Scott Rohde <srohde@igb.illinois.edu>
2013-06-10 15:28:56 -0500

    Reformatted for readability.

--- yieldsview/yieldsview_attached_to_issue_615.sql
+++ yieldsview/yieldsview_attached_to_issue_615.sql
@@ -1,2 +1,36 @@
-drop view if exists yieldsview;
-create view yieldsview as select yields.id as yield_id, yields.citation_id, yields.site_id, yields.treatment_id, sites.sitename as site, sites.city, sites.lat, sites.lon,  species.scientificname, species.genus, citations.author as author, citations.year as cityear, treatments.name as trt, date, month(date) as month, year(date) as year, mean, n, statname, stat, yields.notes, users.name as user, planting, seeding from yields join sites on yields.site_id = sites.id join species on yields.specie_id = species.id join citations on yields.citation_id = citations.id join treatments on yields.treatment_id = treatments.id join users on yields.user_id = users.id join mgmtview on yields.id = mgmtview.yield_id;
+DROP VIEW IF EXISTS yieldsview;
+
+CREATE VIEW yieldsview AS
+       SELECT
+              yields.id AS yield_id,
+              yields.citation_id,
+              yields.site_id,
+              yields.treatment_id,
+              sites.sitename AS site,
+              sites.city,
+              sites.lat,
+              sites.lon,
+              species.scientificname,
+              species.genus,
+              citations.author AS author,
+              citations.year AS cityear,
+              treatments.name AS trt,
+              date,
+              month(date) AS month,
+              year(date) AS year,
+              mean,
+              n,
+              statname,
+              stat,
+              yields.notes,
+              users.name AS user,
+              planting,
+              seeding
+       FROM
+                   yields
+              JOIN sites ON yields.site_id = sites.id
+              JOIN species ON yields.specie_id = species.id
+              JOIN citations ON yields.citation_id = citations.id
+              JOIN treatments ON yields.treatment_id = treatments.id
+              JOIN users ON yields.user_id = users.id
+              JOIN mgmtview ON yields.id = mgmtview.yield_id;
