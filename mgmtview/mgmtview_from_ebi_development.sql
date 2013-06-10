411ae1e05228f0c282a1cec2f6d50dc86ad80881
Scott Rohde <srohde@igb.illinois.edu>
2013-06-10 17:36:42 -0500

    Fixed last commit.

--- mgmtview/mgmtview_from_ebi_development.sql
+++ mgmtview/mgmtview_from_ebi_development.sql
@@ -1,4 +1,4 @@
-CREATE ALGORITHM=UNDEFINED DEFINER=`dlebauer`@`localhost` SQL SECURITY DEFINER VIEW `mgmtview` AS select `yields`.`id` AS `yield_id`,`planting`.`date` AS `planting`,`seeding`.`date` AS `seeding`,`coppice`.`date` AS `coppice` from (((`yields` left join `planting` on((`yields`.`treatment_id` = `planting`.`treatment_id`))) left join `seeding` on((`yields`.`treatment_id` = `seeding`.`treatment_id`))) left join `coppice` on((`yields`.`treatment_id` = `coppice`.`treatment_id`)))CREATE ALGORITHM=UNDEFINED DEFINER=`dlebauer`@`localhost` SQL SECURITY DEFINER VIEW `mgmtview` AS select `yields`.`id` AS `yield_id`,`planting`.`date` AS `planting`,`seeding`.`date` AS `seeding`,`coppice`.`date` AS `coppice` from (((`yields` left join `planting` on((`yields`.`treatment_id` = `planting`.`treatment_id`))) left join `seeding` on((`yields`.`treatment_id` = `seeding`.`treatment_id`))) left join `coppice` on((`yields`.`treatment_id` = `coppice`.`treatment_id`)))CREATE 
+CREATE 
     ALGORITHM = UNDEFINED 
     DEFINER = `DLEBAUER`@`LOCALHOST` 
     SQL SECURITY DEFINER
