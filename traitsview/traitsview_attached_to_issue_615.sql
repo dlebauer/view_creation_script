87b7b23a132de5261d684f023b6f8c8a763034c0
Scott Rohde <srohde@igb.illinois.edu>
2013-06-10 15:02:12 -0500

    Added species.commonname to SELECT list. Formatted for readability. Qualified column names.

--- traitsview/traitsview_attached_to_issue_615.sql
+++ traitsview/traitsview_attached_to_issue_615.sql
@@ -1,2 +1,17 @@
-drop view if exists traitsview;
-create view traitsview as select traits.id as trait_id, traits.citation_id, traits.site_id, traits.treatment_id, sites.sitename as site, sites.city, sites.lat, sites.lon, species.scientificname, species.genus, citations.author as author, citations.year as cityear, treatments.name as trt, date, month(date) as month, year(date) as year, variables.name as trait, mean, n, statname, stat, traits.notes, users.name as user from traits join sites on traits.site_id = sites.id join species on traits.specie_id = species.id join citations on traits.citation_id = citations.id join treatments on traits.treatment_id = treatments.id join variables on traits.variable_id = variables.id join users on traits.user_id = users.id;
+DROP VIEW IF EXISTS traitsview;
+
+CREATE VIEW traitsview AS
+       SELECT traits.id AS trait_id, traits.citation_id, traits.site_id, traits.treatment_id,
+              sites.sitename AS site, sites.city, sites.lat, sites.lon,
+              species.scientificname, species.genus, species.commonname,
+              citations.author AS author, citations.year AS cityear,
+              treatments.name AS trt, traits.date, month(traits.date) AS month,
+              year(traits.date) AS year, traits.dateloc, variables.name AS trait, traits.mean, traits.n,
+              traits.statname, traits.stat, traits.notes, users.name AS user
+       FROM      traits
+            JOIN sites ON traits.site_id = sites.id
+            JOIN species ON traits.specie_id = species.id
+            JOIN citations ON traits.citation_id = citations.id
+            JOIN treatments ON traits.treatment_id = treatments.id
+            JOIN variables ON traits.variable_id = variables.id
+            JOIN users ON traits.user_id = users.id;
