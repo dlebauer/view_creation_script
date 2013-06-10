f996db607d720d6f29b2d65d88a6ce2fea58142f
Scott Rohde <srohde@igb.illinois.edu>
2013-06-10 17:52:51 -0500

    View as output by SHOW CREATE VIEW and cleaned up by MySQL Workbench.

---
+++ mgmtview/coppice_from_ebi_development.sql
@@ -0,0 +1,17 @@
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `DLEBAUER`@`LOCALHOST` 
+    SQL SECURITY DEFINER
+VIEW `coppice` AS
+    SELECT 
+        `mt`.`treatment_id` AS `treatment_id`,
+        `mt`.`management_id` AS `management_id`,
+        `m`.`mgmttype` AS `mgmttype`,
+        `m`.`date` AS `date`,
+        `m`.`level` AS `level`,
+        `m`.`units` AS `units`
+    FROM
+        (`managements` `m`
+        JOIN `managements_treatments` `mt` ON ((`m`.`id` = `mt`.`management_id`)))
+    WHERE
+        (`m`.`mgmttype` = _LATIN1'coppice')
