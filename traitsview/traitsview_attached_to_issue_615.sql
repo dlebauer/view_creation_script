a1adcaf71b483a13520831fcf70212f7fe51afae
Scott Rohde <srohde@igb.illinois.edu>
2013-06-10 15:06:32 -0500

    Reformatted: each column in select list has its own line.

--- traitsview/traitsview_attached_to_issue_615.sql
+++ traitsview/traitsview_attached_to_issue_615.sql
@@ -1,14 +1,34 @@
 DROP VIEW IF EXISTS traitsview;
 
 CREATE VIEW traitsview AS
-       SELECT traits.id AS trait_id, traits.citation_id, traits.site_id, traits.treatment_id,
-              sites.sitename AS site, sites.city, sites.lat, sites.lon,
-              species.scientificname, species.genus, species.commonname,
-              citations.author AS author, citations.year AS cityear,
-              treatments.name AS trt, traits.date, month(traits.date) AS month,
-              year(traits.date) AS year, traits.dateloc, variables.name AS trait, traits.mean, traits.n,
-              traits.statname, traits.stat, traits.notes, users.name AS user
-       FROM      traits
+       SELECT
+              traits.id AS trait_id,
+              traits.citation_id,
+              traits.site_id,
+              traits.treatment_id,
+              sites.sitename AS site,
+              sites.city,
+              sites.lat,
+              sites.lon,
+              species.scientificname,
+              species.genus,
+              species.commonname,
+              citations.author AS author,
+              citations.year AS cityear,
+              treatments.name AS trt,
+              traits.date,
+              month(traits.date) AS month,
+              year(traits.date) AS year,
+              traits.dateloc,
+              variables.name AS trait,
+              traits.mean,
+              traits.n,
+              traits.statname,
+              traits.stat,
+              traits.notes,
+              users.name AS user
+       FROM
+                 traits
             JOIN sites ON traits.site_id = sites.id
             JOIN species ON traits.specie_id = species.id
             JOIN citations ON traits.citation_id = citations.id
