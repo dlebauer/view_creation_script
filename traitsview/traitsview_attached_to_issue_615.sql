6479e5d9395e102e3347069d78692fdb096b3afd
Scott Rohde <srohde@igb.illinois.edu>
2013-06-10 15:19:22 -0500

    SELECT list alterations:        commented out ids for now        changed some aliases (favor more less cryptic names)        coalesed sitename and city

--- traitsview/traitsview_attached_to_issue_615.sql
+++ traitsview/traitsview_attached_to_issue_615.sql
@@ -2,20 +2,19 @@ DROP VIEW IF EXISTS traitsview;
 
 CREATE VIEW traitsview AS
        SELECT
-              traits.id AS trait_id,
-              traits.citation_id,
-              traits.site_id,
-              traits.treatment_id,
-              sites.sitename AS site,
-              sites.city,
+              -- traits.id AS trait_id,
+              -- traits.citation_id,
+              -- traits.site_id,
+              -- traits.treatment_id,
+              COALESE(sites.sitename, sites.city) AS sitename_or_city
               sites.lat,
               sites.lon,
               species.scientificname,
               species.genus,
               species.commonname,
               citations.author AS author,
-              citations.year AS cityear,
-              treatments.name AS trt,
+              citations.year AS citation_year,
+              treatments.name AS treatment,
               traits.date,
               month(traits.date) AS month,
               year(traits.date) AS year,
@@ -26,7 +25,7 @@ CREATE VIEW traitsview AS
               traits.statname,
               traits.stat,
               traits.notes,
-              users.name AS user
+              users.name AS user_name
        FROM
                  traits
             JOIN sites ON traits.site_id = sites.id
