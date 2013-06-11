6f792efaae354c73871da7d688062b50a38610df
Scott Rohde <srohde@igb.illinois.edu>
2013-06-11 10:22:19 -0500

    Adding script to update views.

---
+++ update_views.sh
@@ -0,0 +1,3 @@
+cat traitsview/traitsview_attached_to_issue_615.sql | mysql -u bety bety -p
+cat mgmtview/mgmtview.sql | mysql -u bety bety -p
+cat yieldsview/yieldsview_attached_to_issue_615.sql | mysql -u bety bety -p
