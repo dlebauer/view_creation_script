93b2d174132c0e48086e95fda046798651b6919c
Scott Rohde <srohde@igb.illinois.edu>
2013-06-11 13:59:53 -0500

    Eliminated planting, coppice, and seeding views for now and created new mgmtview that has exactly what we need.

--- mgmtview/mgmtview.sql
+++ mgmtview/mgmtview.sql
@@ -1,4 +1,5 @@
 DROP VIEW IF EXISTS seeding;
+/*
 CREATE VIEW seeding AS
        SELECT
               mt.treatment_id,
@@ -11,8 +12,9 @@ CREATE VIEW seeding AS
                    managements AS m
               JOIN managements_treatments AS mt ON m.id = mt.management_id
        WHERE m.mgmttype IN ('seeding', 'seed_density', 'row_spacing');
-                     
+*/                     
 DROP VIEW IF EXISTS coppice;
+/*
 CREATE VIEW coppice AS
        SELECT
               mt.treatment_id,
@@ -25,8 +27,9 @@ CREATE VIEW coppice AS
                    managements AS m
               JOIN managements_treatments AS mt ON m.id = mt.management_id
        WHERE m.mgmttype IN ('coppice');
-
+*/
 DROP VIEW IF EXISTS planting;
+/*
 CREATE VIEW planting AS
        SELECT 
               mt.treatment_id,
@@ -39,8 +42,9 @@ CREATE VIEW planting AS
               managements AS m
               JOIN managements_treatments AS mt ON m.id = mt.management_id
        WHERE m.mgmttype in ('planting');
-
+*/
 DROP VIEW IF EXISTS mgmtview;  
+/*
 CREATE VIEW mgmtview AS
        SELECT
               yields.id AS yield_id,
@@ -51,3 +55,15 @@ CREATE VIEW mgmtview AS
               LEFT OUTER JOIN planting ON yields.treatment_id = planting.treatment_id
               LEFT OUTER JOIN seeding ON yields.treatment_id = seeding.treatment_id
               LEFT OUTER JOIN coppice ON yields.treatment_id = coppice.treatment_id; 
+*/
+
+CREATE VIEW mgmtview AS
+       SELECT
+            yields.id AS yield_id,
+            m.units
+       FROM yields
+            LEFT JOIN (
+                 managements AS m
+                 JOIN managements_treatments AS mt ON m.id = mt.management_id)
+            ON yields.treatment_id = mt.treatment_id;
+       
